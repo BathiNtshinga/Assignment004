b9e7730679c8e004cdc84da4e593f293042a1042
BathiNtshinga <60979642+BathiNtshinga@users.noreply.github.com>
2020-03-09 23:07:25 +0200

    Initial Commit

--- src/main/java/org/ntshinga/App.java
+++ src/main/java/org/ntshinga/App.java
@@ -1,22 +1,45 @@
 package org.ntshinga;
 import java.util.*;
-import java.util.Arrays.*;
+import java.util.Set.*;
+import java.util.List;
+import java.util.List.*;
 /**
- * Hello world!
+ * ArrayList, HashMap and HashSet
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        ArrayList<String> list=new ArrayList<String>();
-        list.add("Bathi");
-        list.add("Fabian");
-        list.add("Ncebakazi");
+public class App {
+    public static void main(String[] args) {
+        Student s1 = new Student("Bathi Ntshinga");
+        Student s2 = new Student("Ncebakazi Dingwayo");
+        Student s3 = new Student("Fabian Botha");
+        Student s4 = new Student("Sonwabo Glen ");
 
-        Iterator itr=list.iterator();
-        while(itr.hasNext()){
-            System.out.println(itr.next());
-        }
+        List<Student> studentList = new ArrayList<>();
+        Set<Student> studentSet = new HashSet<>();
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        studentList.add(s1);
+        studentList.add(s2);
+        studentList.add(s3);
+        studentList.add(s4);
+
+        System.out.println(studentList);
+
+        studentSet.add(s1);
+        studentSet.add(s2);
+        studentSet.add(s3);
+        studentSet.add(s4);
+
+        System.out.println(studentSet);
+
+        map.put("Bathi Ntshinga", "Bathi01");
+        map.put("Fabian Botha", "password2020");
+        map.put("Ncebakazi Dingwayo", "ncebsy1985");
+        map.put("Sonwabo Glen", "summer@2");
+
+        System.out.println(map);
+
     }
 }
+
+
