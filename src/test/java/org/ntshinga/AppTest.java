718646d2e3ebd6c215c5a658850acc5a8413f1ec
BathiNtshinga <60979642+BathiNtshinga@users.noreply.github.com>
2020-03-10 12:34:12 +0200

    Initial Commit

--- src/test/java/org/ntshinga/AppTest.java
+++ src/test/java/org/ntshinga/AppTest.java
@@ -1,28 +1,38 @@
 package org.ntshinga;
 import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.rules.Timeout;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
 import org.junit.Test;
-
+import org.junit.rules.Timeout;
+import static org.hamcrest.core.Is.is;
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
+    private Student studentName;
+    private Student stud;
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(2000);
 
     @Test
-    public void testStudentName(){
-        Student s1 = new Student();
-        s1.name = "Bathi Ntshinga";
-        Student s2 = s1;
-        Assert.assertEquals(s1, s2);
-        Assert.assertSame(s1,s2);
-
-        Student s3 = new Student();
-        s3.name = "Fabian Botha";
-        Assert.assertEquals(s1,s3);
-        Assert.assertSame(s1,s3);
+    public void testIdentity(){
+        Student studentName = new Student();
+        Student stud = new Student();
+        Assert.assertNotSame(studentName, stud);
     }
 
+    @Test
+    public void testFail(){
+        int i; i=(3*2);
+        assertThat(String.valueOf(3*2),6,is(i));
+    }
+
+    public void testDisable(){
+        System.out.println("The test will not execute");
+    }
 
 }
